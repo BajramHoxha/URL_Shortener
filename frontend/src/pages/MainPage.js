2f078719307f9ceae43f79d0418bef81bd3c9cf0
“BajramHoxha” <b.hoxhaa@gmail.com>
2021-03-02 10:58:26 +0100

    Fixed styling issues and added icon

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -5,7 +5,8 @@ export default (props) => (
 	<div className="row justify-content-center main-container" id="Page-container">
 		<div className="col-12 col-md-10 col-lg-8">
 			<div className="row justify-content-center">
-				<div className="col-9 col-md-9 form-container">
+				<div className="col-7 col-md-7 form-container">
+				    <i class="fas fa-cut cut-icon fa-2x"></i>
 				    <span class="url-shortener-headline">URL Shortener</span>
                     <Form />
 				</div>
